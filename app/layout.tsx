c6f6862e3a5d743ac700af9b97d7fb90e170b718
SirBM201 <bms.concept@hotmail.com>
2026-03-21 17:14:37 +0300

    Add Meta domain verification tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,16 @@ import Providers from "@/components/Providers";
 export const metadata: Metadata = {
   title: "NaijaTax Guide",
   description: "NaijaTax Guide Web Portal",
+  other: {
+    "facebook-domain-verification": "1da7tqv44h2lu0wyv7vmm9o4eh9us6",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
